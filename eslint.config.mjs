82bbce7af38ede4cdf49ea76bcc8f603bb95ff5b
Palicz Dániel <hdtoki@gmail.com>
2025-02-27 18:54:59 +0100

    feat: initial project setup with Next.js, TypeScript, and Tailwind CSS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,40 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintPluginUnicorn.configs.recommended,
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "plugin:prettier/recommended",
+      "plugin:playwright/recommended",
+      "plugin:import/recommended",
+    ],
+    plugins: ["simple-import-sort"],
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "unicorn/no-array-callback-reference": "off",
+      "unicorn/no-array-for-each": "off",
+      "unicorn/no-array-reduce": "off",
+      "unicorn/prevent-abbreviations": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+          singleQuote: true,
+          trailingComma: "all",
+        },
+      ],
+    },
+    overrides: [
+      {
+        files: ["*.js"],
+        rules: {
+          "unicorn/prefer-module": "off",
+        },
+      },
+    ],
+  }),
 ];
 
 export default eslintConfig;
