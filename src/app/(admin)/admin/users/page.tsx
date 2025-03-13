ee82664fa9c547e2505b0ef0c08bf0044c06dd80
Palicz Dániel <hdtoki@gmail.com>
2025-03-14 00:14:10 +0100

    fix(admin):  added dynamic rendering to users page to prevent prerender errors

--- src/app/(admin)/admin/users/page.tsx
+++ src/app/(admin)/admin/users/page.tsx
@@ -6,6 +6,8 @@ import { HydrateClient, trpc } from '@/trpc/server';
 import { UsersClient } from './client';
 import { TableSkeleton } from './table-skeleton';
 
+export const dynamic = 'force-dynamic';
+
 export default async function UsersAdminPage() {
   void trpc.users.getMany.prefetch();
 
